3690817d817852f7e6e2fda28271f4459b150ca8
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-04 20:34:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    words.select {|word| is_anagram?(word)}
+  end
+
+  def is_anagram?(word)
+    word.chars.sort == anagram.chars.sort
+  end
+
+end
