f4efa97d51f6f8b63fab7cd8cb3dca4b58b285e9
joaquimbarreto <joaquim@barreto.co>
2018-11-11 16:18:59 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    @@all = []
+
+    def initialize(word)
+        @word = word
+        @@all << self    
+    end
+
+    def self.all
+        @@all
+    end
+
+    def match(words)
+        words.select {|w| w.split("").sort == @word.split("").sort}
+    end
+
+end
